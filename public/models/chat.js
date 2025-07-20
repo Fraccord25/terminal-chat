714fb6895d6b97ae07e336c8b749bfc0daea85d6
Fajar Ramadhan <114432075+Fraccord25@users.noreply.github.com>
2025-07-20 20:54:00 +0700

    Delete public directory

--- public/models/chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const chatSchema = new mongoose.Schema({
-    username: String,
-    text: String,
-    timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Chat', chatSchema);
